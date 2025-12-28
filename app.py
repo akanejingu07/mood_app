f78e18b2034b3f35ba4cbf6e811336143cad3c00
akane.jingu07 <akane.jingu07@gmail.com>
2025-12-28 18:56:50 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -15,12 +15,22 @@ def get_db_connection():
     database_url = os.environ.get("DATABASE_URL")
     if not database_url:
         raise RuntimeError("DATABASE_URL is not set")
-    return psycopg2.connect(
-        database_url,
-        cursor_factory=RealDictCursor,
-        sslmode="require",
-        connect_timeout=5
-    )
+
+    # Renderかどうかで分岐
+    if "render.com" in database_url or "sslmode" in database_url:
+        return psycopg2.connect(
+            database_url,
+            cursor_factory=RealDictCursor,
+            sslmode="require",
+            connect_timeout=5
+        )
+    else:
+        return psycopg2.connect(
+            database_url,
+            cursor_factory=RealDictCursor,
+            connect_timeout=5
+        )
+
 
 # --------------------
 # 初期テーブル作成（手動実行推奨）
